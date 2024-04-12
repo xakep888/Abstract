5d88c4a233097070d1f4f0851bbe0b6980eb5c78
Nikita <topolik777@mail.ru>
2024-04-12 18:31:18 +0300

    Main

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Human hm = new Human();
+        hm.name = "Roman";
+
+        Market market = new Market();
+
+        market.acceptToMarket(hm);
+        market.takeInQueue(hm);
+        market.giveOrder();
+        market.takeOrder();
+        market.releaseFromQueue();
+        market.releaseFromMarket(hm);
+        market.update();
+
+    }
+}
