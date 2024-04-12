fe4af20669c3151bc605c3e28def2ca4efe92a8c
Nikita <topolik777@mail.ru>
2024-04-12 18:30:15 +0300

    Buyer

---
+++ Buyer.java
@@ -0,0 +1,9 @@
+public abstract class Buyer implements BuyerBehavior {
+    protected String name;
+    protected boolean isMakeOrderClass;
+    protected boolean isTakeOrderClass;
+
+    abstract String getName();
+
+
+}
