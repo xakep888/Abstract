d072a692b206ddc7bf26f8f5ffcd71383f64c48d
Nikita <topolik777@mail.ru>
2024-04-12 18:31:04 +0300

    Human

---
+++ Human.java
@@ -0,0 +1,27 @@
+public class Human extends Buyer {
+
+    @Override
+    String getName() {
+        return name;
+    }
+
+    @Override
+    public void setMakeOrder() {
+        isMakeOrderClass = true;
+    }
+
+    @Override
+    public void setTakeOrder() {
+        isTakeOrderClass = true;
+    }
+
+    @Override
+    public boolean isMakeOrder() {
+         return isMakeOrderClass;
+    }
+
+    @Override
+    public boolean isTakeOrder() {
+        return isTakeOrderClass;
+    }
+}
